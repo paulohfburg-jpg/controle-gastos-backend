cf0b332e7ebe2b1e9eb1cb756fef4b60dd3d516c
paulohfburg-jpg <pauloh.f.burg@gmail.com>
2025-08-13 10:24:03 -0300

    Update app.py

--- app.py
+++ app.py
@@ -312,4 +312,7 @@ def index():
     return send_from_directory(app.static_folder, 'index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))  # Render define PORT, localmente usa 5000
+    app.run(host="0.0.0.0", port=port, debug=True)
+
